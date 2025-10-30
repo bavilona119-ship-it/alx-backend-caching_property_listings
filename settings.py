4283444cfac809ccb3e3c2a936fdbc0c13cd6010
bavilona119-ship-it <bavilona119@gmail.com>
2025-10-30 16:32:17 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'property_db',
+        'USER': 'property_user',
+        'PASSWORD': 'property_pass',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
